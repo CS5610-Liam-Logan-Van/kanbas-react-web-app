10ffd2b11b9a39e596540fc3b926f99ab7a3225b
Logan Willans <156117377+loganjwillans@users.noreply.github.com>
2024-07-29 14:11:12 -0400

    a4 lab4

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -32,12 +32,23 @@ import Square from "./Square";
 import Highlight from "./Highlight";
 import AddPathParameters from "./AddPathParameters";
 import PathParameters from "./PathParameters";
+import {useSelector} from "react-redux";
 
 export default function Lab3() {
     console.log('Hello World!');
+    const {todos} = useSelector((state: any) => state.todosReducer);
+
     return (
         <div id="wd-lab3" className="container-fluid">
             <h3>Lab 3</h3>
+            <ul className="list-group">
+                {todos.map((todo: any) => (
+                    <li className="list-group-item" key={todo.id}>
+                        {todo.title}
+                    </li>
+                ))}
+            </ul>
+            <hr/>
             <VariablesAndConstants/>
             <VariableTypes/>
             <BooleanVariables/>
@@ -72,9 +83,11 @@ export default function Lab3() {
             <Square>4</Square>
             <hr/>
             <Highlight>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum,
+                saepe
                 totam
-                vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident
+                vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores,
+                provident
                 voluptates.
             </Highlight>
             <PathParameters/>
