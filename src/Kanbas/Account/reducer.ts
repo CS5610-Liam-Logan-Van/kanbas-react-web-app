0aab563f1eee2028dbac2aba19a4cc55b980e645
Logan Willans <156117377+loganjwillans@users.noreply.github.com>
2024-08-18 16:50:27 -0400

    askldaklsdjlkaslkjnfas

--- src/Kanbas/Account/reducer.ts
+++ src/Kanbas/Account/reducer.ts
@@ -1,20 +1,7 @@
-import {createSlice, current} from "@reduxjs/toolkit";
-
-interface User {
-    _id: string;
-    username: string;
-    password: string;
-    firstName: string;
-    email: string;
-    lastName: string;
-    role: string;
-    section: string;
-    dob: string;
-}
-
+import {createSlice} from "@reduxjs/toolkit";
 
 const initialState = {
-    currentUser: [] as User[],
+    currentUser: null,
 };
 const accountSlice = createSlice({
     name: "account",
@@ -23,17 +10,7 @@ const accountSlice = createSlice({
         setCurrentUser: (state, action) => {
             state.currentUser = action.payload;
         },
-        updateUser: (state, {payload: user}) => {
-            state.currentUser = state.currentUser.map((u: any) => {
-                    if (u._id === user._id) {
-                        return user;
-                    } else {
-                        return u;
-                    }
-                }
-            );
-        },
     },
 });
-export const {setCurrentUser, updateUser} = accountSlice.actions;
-export default accountSlice.reducer
+export const {setCurrentUser} = accountSlice.actions;
+export default accountSlice.reducer;
